ea24df5efb7e98c76563d54b1413aca82b39a5aa
PeanutSoup55 <114015859+SSCsoup@users.noreply.github.com>
2024-12-07 20:59:20 -0500

    added reactive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logoAO from '../images/logoAO.png'
-
+import {IoSearch } from 'react-icons/io5';
+import { useState } from 'react';
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <div className="navbar-left">
+     
+     <div className="logo">
+     <Link to="/">
         <img src={logoAO} alt="Logo" className="navbar-logo" />
+      </Link>
+     </div>
+
+      
+
+      <div className='menu' onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
       
-      <div className="navbar-center">
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/Sponsor">Sponsor</Link></li>
-          <li><Link to="/Events">Events</Link></li>
-          <li><Link to="/Contact">Contact</Link></li>
-        </ul>
-      </div>
+
+      <ul className={menuOpen ? "open" : ""}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Sponsor">Sponsor</Link></li>
+        <li><Link to="/Events">Events</Link></li>
+        <li><Link to="/Contact">Contact</Link></li>
+      </ul>
 
       <div className="navbar-right">
         <div className="search-bar">
           <input type="text" placeholder="Search..." />
           <button type="submit">
-            <i className="fas fa-search"></i>
+            <IoSearch/>
           </button>
         </div>
       </div>
